7fae8227550cac335a7a5e6da03d2c10ea4655e6
ojyrkinen <ojyrkinen@student.ethz.ch>
2023-05-03 08:18:33 +0000

     Fixed some test-cases

--- analysis/src/test/java/ch/ethz/rse/integration/tests/NonNeg_If_Test_1_Safe.java
+++ analysis/src/test/java/ch/ethz/rse/integration/tests/NonNeg_If_Test_1_Safe.java
@@ -3,7 +3,7 @@ package ch.ethz.rse.integration.tests;
 import ch.ethz.rse.Store;
 
 // expected results:
-// NON_NEGATIVE SAFE
+// NON_NEGATIVE UNSAFE
 // FITS_IN_TROLLEY SAFE
 // FITS_IN_RESERVE SAFE
 
