191a57598dd3b1b9034d5ff36122520dac130252
ojyrkinen <ojyrkinen@student.ethz.ch>
2023-06-03 12:12:29 +0000

    Added Trolley_Test_Unsafe_7

--- analysis/src/test/java/ch/ethz/rse/integration/SpecificExampleIT.java
+++ analysis/src/test/java/ch/ethz/rse/integration/SpecificExampleIT.java
@@ -21,16 +21,16 @@ public class SpecificExampleIT {
 	 */
 	@Test
 	void specificTest() {
-		String packageName = "ch.ethz.rse.integration.tests.Trolley_Test_Unsafe";
+		String packageName = "ch.ethz.rse.integration.tests.Trolley_Test_Unsafe_7";
 		VerificationProperty verificationTask = VerificationProperty.NON_NEGATIVE;
-		boolean expectedIsSafe = true;
+		boolean expectedIsSafe = false;
 		VerificationTestCase t = new VerificationTestCase(packageName, verificationTask, expectedIsSafe);
 		SpecificExampleIT.testOnExample(t);
 	}
 
 	@Test
 	void specificTest1() {
-		String packageName = "ch.ethz.rse.integration.tests.Trolley_Test_Unsafe";
+		String packageName = "ch.ethz.rse.integration.tests.Trolley_Test_Unsafe_7";
 		VerificationProperty verificationTask = VerificationProperty.FITS_IN_TROLLEY;
 		boolean expectedIsSafe = false;
 		VerificationTestCase t = new VerificationTestCase(packageName, verificationTask, expectedIsSafe);
@@ -39,9 +39,9 @@ public class SpecificExampleIT {
 
 	@Test
 	void specificTest2() {
-		String packageName = "ch.ethz.rse.integration.tests.Trolley_Test_Unsafe";
+		String packageName = "ch.ethz.rse.integration.tests.Trolley_Test_Unsafe_7";
 		VerificationProperty verificationTask = VerificationProperty.FITS_IN_RESERVE;
-		boolean expectedIsSafe = false;
+		boolean expectedIsSafe = true;
 		VerificationTestCase t = new VerificationTestCase(packageName, verificationTask, expectedIsSafe);
 		SpecificExampleIT.testOnExample(t);
 	}
